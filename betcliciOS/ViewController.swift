59fd9ab34c8156c7f3594777574c504f4d970f88
Apple <apple@macintosh-89.local>
2017-03-24 09:15:32 +0100

    Initial Commit

---
+++ betcliciOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  betcliciOS
+//
+//  Created by Apple on 24/03/2017.
+//  Copyright © 2017 M2i. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
