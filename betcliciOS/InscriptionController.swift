3e27beb90465a4ee23f268782bbad69b62177e44
yann cap <yann.cap@live.com>
2017-03-24 12:03:30 +0100

    ecran d'inscription et des controles associés dans le controller

--- betcliciOS/InscriptionController.swift
+++ betcliciOS/InscriptionController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class InscriptionController: UIViewController {
 
+    @IBOutlet weak var lastnameText: UITextField!
+    @IBOutlet weak var firstnameText: UITextField!
+    @IBOutlet weak var birthdateText: UITextField!
+    @IBOutlet weak var addressText: UITextField!
+    @IBOutlet weak var cityText: UITextField!
+    @IBOutlet weak var countryText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var validateEmailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var validatePasswordText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func Signup(_ sender: Any) {
     }
 
 }
